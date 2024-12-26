c54767ae16f15e6f8f892cb107197ed551789706
alaminsk99 <alaminsaikh2525@gmail.com>
2024-12-26 17:37:08 +0530

    First and show type okay.

---
+++ leranning/first.js
@@ -0,0 +1,16 @@
+/// Variable 
+
+const accountId = 1292818;
+
+let myEmail = "Saikha752@gmail.com"
+var password = "7237623"
+
+myName = "Alamin Sk"
+
+myEmail = "kasjsa@gmaial.com"
+password = "87"
+myName = "Rohit"
+
+console.log(accountId);
+
+console.table([accountId,myEmail,password,myName]);
