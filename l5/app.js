2c96c5d2b4c2b8b325743a3f46ef39ae2c5ed2db
alaminsk99 <alaminsaikh2525@gmail.com>
2025-01-07 07:02:08 +0530

    all

---
+++ l5/app.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null>=0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === is strict check the variable.
+
+// console.log("2" === 2);
+
+
---
+++ l6/app.js
@@ -0,0 +1,38 @@
+
+
+const number = 23.34;
+const add = 23;
+const myName = "Alamin Sk";
+let email;
+const bigInt = 2442n;
+const isLoggedIn = false;
+const outSideTemp = null;
+
+const id = Symbol("1313");
+const anotherId = Symbol('1313');
+
+// console.log(id === anotherId);
+
+
+const sudentName = ["Ram", "Sam","Sod","Jod","Jansi"];
+
+let myObj = {
+    name: "Alamin Sk",
+    age: 19,
+};
+
+const myFunction = function(){};
+
+console.table([
+    typeof myFunction, 
+    typeof number, 
+    typeof add, 
+    typeof myName, 
+    typeof email, 
+    typeof bigInt, 
+    typeof isLoggedIn, 
+    typeof outSideTemp, 
+    typeof id,
+]);
+
+
