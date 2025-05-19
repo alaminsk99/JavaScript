93804462bddb9d640dbf95936bed06dbe6bfb96e
Alamin Sk <alaminsaikh2525@gmail.com>
2025-05-19 11:53:07 +0000

    Fibonacci series up to N terms.

--- Problem/chatgtp_problem.js
+++ Problem/chatgtp_problem.js
@@ -1,22 +1,39 @@
-//Print all prime numbers between 1 and N.
+/// Fibonacci series up to N terms.
+
+
+/* First 2 terms are always: 0 1
+
+From the 3rd term, each term = sum of previous 2
+
+Update the last two numbers every time
+
+| i  | a  | b  | Output |
+| -- | -- | -- | ------ |
+| 1  | 0  | 1  | 0      |
+| 2  | 1  | 1  | 1      |
+| 3  | 1  | 2  | 1      |
+| 4  | 2  | 3  | 2      |
+| 5  | 3  | 5  | 3      |
+| 6  | 5  | 8  | 5      |
+| 7  | 8  | 13 | 8      |
+| 8  | 13 | 21 | 13     |
+| 9  | 21 | 34 | 21     |
+| 10 | 34 | 55 | 34     |
+
+
+*/
 
 const N = 10;
 
-if(N===1){
-    return;
-}else{
-    for(let i=2; i<=N; i++){
-        let isPrime = true;
+let a = 0, b = 1;
 
-        for(let j=2; j<= Math.sqrt(i); j++){
-            if(i%j===0){
-                isPrime = false;
-                break;
-            }
-        }
+console.log("Fibonacci Series up to " + N + " terms:");
 
-        if(isPrime){
-            console.log(i);
-        }
-    }
+
+for(let i =1; i<=N; i++){
+    console.log(a);
+    let next = a+b;
+    a=b;
+    b=next;
 }
+
