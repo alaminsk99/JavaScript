f671b1fdd271994076503439d0b097b4ef91c1fc
Alamin Sk <alaminsaikh2525@gmail.com>
2025-05-20 02:44:51 +0000

    Sum of digits of a number.

--- Problem/chatgtp_problem.js
+++ Problem/chatgtp_problem.js
@@ -1,39 +1,16 @@
-/// Fibonacci series up to N terms.
+//Sum of digits of a number.
 
 
-/* First 2 terms are always: 0 1
+// ----> 136= 1+3+6=10
 
-From the 3rd term, each term = sum of previous 2
+const num = 73264356323;
+let N = num;
+let sum =0;
 
-Update the last two numbers every time
-
-| i  | a  | b  | Output |
-| -- | -- | -- | ------ |
-| 1  | 0  | 1  | 0      |
-| 2  | 1  | 1  | 1      |
-| 3  | 1  | 2  | 1      |
-| 4  | 2  | 3  | 2      |
-| 5  | 3  | 5  | 3      |
-| 6  | 5  | 8  | 5      |
-| 7  | 8  | 13 | 8      |
-| 8  | 13 | 21 | 13     |
-| 9  | 21 | 34 | 21     |
-| 10 | 34 | 55 | 34     |
-
-
-*/
-
-const N = 10;
-
-let a = 0, b = 1;
-
-console.log("Fibonacci Series up to " + N + " terms:");
-
-
-for(let i =1; i<=N; i++){
-    console.log(a);
-    let next = a+b;
-    a=b;
-    b=next;
+while(N !=0){
+    let digit = N%10;
+    sum = sum+digit;
+    N = Math.floor(N/10);                   
 }
 
+console.log(`The Sum of digit of ${num} is ${sum}`);
