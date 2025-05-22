edef2f9a8ff051a412aa497d614b8d12a9bb3aa4
Alamin Sk <alaminsaikh2525@gmail.com>
2025-05-22 11:34:07 +0000

    Pattern printing problem---- Star

--- Problem/chatgtp_problem.js
+++ Problem/chatgtp_problem.js
@@ -1,17 +1,47 @@
-// Convert decimal to binary using loops.
+//Pattarn prolems using loops -----------> Star
 
 
 
+let star = "";
 
+// Top part
+for (let i = 1; i <= 5; i++) {
+  // Left stars
+  for (let j = 1; j <= i; j++) {
+    star += "*";
+  }
 
-let a = 13;
-let num = a;
-let binary = '';
+  // Center spaces
+  for (let s = 1; s <= (10 - 2 * i); s++) {
+    star += " ";
+  }
 
+  // Right stars
+  for (let j = 1; j <= i; j++) {
+    star += "*";
+  }
 
-while(num>0){
-    binary = (num%2)+ binary;
-    num = Math.floor(num/2);
+  star += "\n";
 }
 
-console.log(`Binary of ${a} is ${binary}`);
+// Bottom part
+for (let i = 5; i >= 1; i--) {
+  // Left stars
+  for (let j = 1; j <= i; j++) {
+    star += "*";
+  }
+
+  // Center spaces
+  for (let s = 1; s <= (10 - 2 * i); s++) {
+    star += " ";
+  }
+
+  // Right stars
+  for (let j = 1; j <= i; j++) {
+    star += "*";
+  }
+
+  star += "\n";
+}
+
+console.log(star);
