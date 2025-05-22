51e7bd255f8d6f4089ba3035f544674edc4febac
Alamin Sk <alaminsaikh2525@gmail.com>
2025-05-22 12:06:52 +0000

    Pyramid pattern problem ----> Pyramid

--- Problem/chatgtp_problem.js
+++ Problem/chatgtp_problem.js
@@ -1,47 +1,34 @@
-//Pattarn prolems using loops -----------> Star
+//Pattern problem of pyramid
+/*
+
+
+    *                       --> star = 1 , sp = 4             { sp= 5-i and star (2*i-1)    }
+   ***                      --> star = 3,  sp = 3 
+  *****                     --> star = 5,  sp = 2,
+ *******                    --> star = 7,  sp = 1,
+*********                   --> star = 9,  sp = 0
 
 
 
-let star = "";
 
-// Top part
-for (let i = 1; i <= 5; i++) {
-  // Left stars
-  for (let j = 1; j <= i; j++) {
-    star += "*";
-  }
+*/
 
-  // Center spaces
-  for (let s = 1; s <= (10 - 2 * i); s++) {
-    star += " ";
-  }
 
-  // Right stars
-  for (let j = 1; j <= i; j++) {
-    star += "*";
-  }
 
-  star += "\n";
+let pattern = "";
+
+
+for(let i = 1; i<=5; i++){
+    // Spaces
+
+    for(let j=1; j<= (5-i); j++){
+        pattern += " ";
+    }
+    for(let k = 1;k<=(2*i-1); k++){
+        pattern += "*";
+    }
+    pattern +="\n";
 }
 
-// Bottom part
-for (let i = 4; i >= 1; i--) {
-  // Left stars
-  for (let j = 1; j <= i; j++) {
-    star += "*";
-  }
 
-  // Center spaces
-  for (let s = 1; s <= (10 - 2 * i); s++) {
-    star += " ";
-  }
-
-  // Right stars
-  for (let j = 1; j <= i; j++) {
-    star += "*";
-  }
-
-  star += "\n";
-}
-
-console.log(star);
+console.log(`${pattern}`)
