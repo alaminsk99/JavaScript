238d9919bc9162b8624120c737e9515efbb3a0aa
Alamin Sk <alaminsaikh2525@gmail.com>
2025-05-25 06:42:04 +0000

    An problem arrays method.

--- arrays problems/saradha_mams_problems.js
+++ arrays problems/saradha_mams_problems.js
@@ -1,17 +1,29 @@
+// Create an array to store companies .
 
 
-// Item \s prices problems.
+
+let companies = ["Bloomberg", "Microsoft","Uber","Google","IBM","Netflix"];
 
 
-let prices = [250, 645, 300,900, 50];
+console.log(companies)
+// Remove the first company from array
+
+companies.splice(0,1);
 
 
-for ( let i=0; i<prices.length; i++){
-    // First way
-   // prices[i] = prices[i]-(prices[i]/10);
-    // Second way
-   prices[i] -= (prices[i]/10);
-    
-}
 
-console.log(prices);
+console.log(companies)
+
+// Remove Uber and Add Ola in its place.
+
+companies.splice(1,1,"Ola");
+
+
+
+console.log(companies)
+
+// Add Amazon at the end.
+
+companies.push("Amazon");
+
+console.log(companies)
