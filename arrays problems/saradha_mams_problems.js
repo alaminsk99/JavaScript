9ef22292ada0128a6656a5ed9d04a3b500a8e92f
Alamin Sk <alaminsaikh2525@gmail.com>
2025-05-29 02:12:22 +0000

    This is another one of Solusan

--- arrays problems/saradha_mams_problems.js
+++ arrays problems/saradha_mams_problems.js
@@ -1,24 +1,23 @@
-//Find the maximum and minimum element in an array.
+//Calculate the sum of all elements in an array.
 
 
 
 
-let arr =[3,1,3,5,6,9];
+let arr =[3,1,3];
 
 
-let max = arr[0]
-let min = arr[0]
+
+let sum = 0;
 
 
 
 for(let i =0; i< arr.length;i++){
-    if(arr[i]<min){
-        min = arr[i];
-    }
-    if(arr[i]>max){
-        max = arr[i];
-    }
+    
+    sum += arr[i];
 }
 
 
-console.log(`Maximum: ${max} and Minimum: ${min} `);
+let sums = arr.reduce((acc, curr)=> acc+curr,0);
+
+
+console.log(`Sum of the array elements: ${sums}`);
