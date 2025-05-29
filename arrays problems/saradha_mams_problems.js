b9971ef2c6191eba8bc3e2467007a9f4062586ca
Alamin Sk <alaminsaikh2525@gmail.com>
2025-05-29 02:05:44 +0000

    //Find the maximum and minimum element in an array. ---------> Another method

--- arrays problems/saradha_mams_problems.js
+++ arrays problems/saradha_mams_problems.js
@@ -3,11 +3,22 @@
 
 
 
-let arr =[2,3,5,6,9];
+let arr =[3,1,3,5,6,9];
 
 
-let max = Math.max(...arr);
-let min = Math.min(...arr);
+let max = arr[0]
+let min = arr[0]
+
+
+
+for(let i =0; i< arr.length;i++){
+    if(arr[i]<min){
+        min = arr[i];
+    }
+    if(arr[i]>max){
+        max = arr[i];
+    }
+}
 
 
 console.log(`Maximum: ${max} and Minimum: ${min} `);
